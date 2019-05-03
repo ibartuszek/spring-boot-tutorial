bc82f1fccd1f57b97291602d5798ff925c8d62ac
Istvan Bartuszek <v-ibartuszek@hotels.com>
2019-05-03 15:02:58 +0200

    11 - Logging

--- src/main/java/com/example/springboottutorial/HelloController.java
+++ src/main/java/com/example/springboottutorial/HelloController.java
@@ -1,11 +1,15 @@
 package com.example.springboottutorial;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
 
     @Value("${spring.application.name:default_name}")
     private String name;
@@ -15,8 +19,11 @@ public class HelloController {
 
     @RequestMapping(value = "/")
     public String hello() {
-        System.out.println(missingProperty);
+        HelloController.LOGGER.info(missingProperty);
         return name;
     }
+
+    // [%d{yyyy-MM-dd'T'HH:mm:ss.sss'Z'}] [%C] [%t] [%L] [%-5p] %m%n
+    // [2019-05-03T14:54:51.051Z] [com.example.springboottutorial.HelloController] [http-nio-9091-exec-1] [22] [INFO ] there_is_no_value
 
 }
