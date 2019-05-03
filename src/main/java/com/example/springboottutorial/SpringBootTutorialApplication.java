6a2d099e85ba74ad3a39e6ef1d55eb28a310e7c4
Istvan Bartuszek <v-ibartuszek@hotels.com>
2019-05-03 09:52:23 +0200

    4 - add "Hello world" endpoint to main class, modify pom: java11 main-class

--- src/main/java/com/example/springboottutorial/SpringBootTutorialApplication.java
+++ src/main/java/com/example/springboottutorial/SpringBootTutorialApplication.java
@@ -2,12 +2,20 @@ package com.example.springboottutorial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootTutorialApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootTutorialApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTutorialApplication.class, args);
+    }
+
+    @RequestMapping(value = "/")
+    public String hello() {
+        return "Hello World";
+    }
 
 }
