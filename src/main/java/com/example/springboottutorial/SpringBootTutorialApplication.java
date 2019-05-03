cffd3d945d96201988b74525990cfb6e781d37e3
Istvan Bartuszek <v-ibartuszek@hotels.com>
2019-05-03 13:51:56 +0200

    9 - implement ApplicationRunner

--- src/main/java/com/example/springboottutorial/SpringBootTutorialApplication.java
+++ src/main/java/com/example/springboottutorial/SpringBootTutorialApplication.java
@@ -1,20 +1,25 @@
 package com.example.springboottutorial;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootTutorialApplication extends SpringBootServletInitializer {
+public class SpringBootTutorialApplication /* extends SpringBootServletInitializer */ implements ApplicationRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootTutorialApplication.class, args);
     }
 
+    //    @Override
+    //    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    //        return application.sources(SpringBootTutorialApplication.class);
+    //    }
+
     @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(SpringBootTutorialApplication.class);
+    public void run(ApplicationArguments arg0) throws Exception {
+        System.out.println("Hello World from Application Runner");
     }
 
 }
