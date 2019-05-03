ee1f1df2f05323180278a75b0a25bd158478acaa
Istvan Bartuszek <v-ibartuszek@hotels.com>
2019-05-03 12:54:53 +0200

    5 - add Tomcat deploy, refactor

--- src/main/java/com/example/springboottutorial/SpringBootTutorialApplication.java
+++ src/main/java/com/example/springboottutorial/SpringBootTutorialApplication.java
@@ -2,20 +2,19 @@ package com.example.springboottutorial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-@RestController
-public class SpringBootTutorialApplication {
+public class SpringBootTutorialApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootTutorialApplication.class, args);
     }
 
-    @RequestMapping(value = "/")
-    public String hello() {
-        return "Hello World";
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootTutorialApplication.class);
     }
 
 }
