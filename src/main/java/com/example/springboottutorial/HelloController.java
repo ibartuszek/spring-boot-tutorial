f576126d9fb56fb2fecd3d4040d85cea7ec5f3e8
Istvan Bartuszek <v-ibartuszek@hotels.com>
2019-05-03 14:39:45 +0200

    10 - property files

--- src/main/java/com/example/springboottutorial/HelloController.java
+++ src/main/java/com/example/springboottutorial/HelloController.java
@@ -1,14 +1,22 @@
 package com.example.springboottutorial;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Value("${spring.application.name:default_name}")
+    private String name;
+
+    @Value("${missing.property:there_is_no_value}")
+    private String missingProperty;
+
     @RequestMapping(value = "/")
     public String hello() {
-        return "Hello World";
+        System.out.println(missingProperty);
+        return name;
     }
 
 }
