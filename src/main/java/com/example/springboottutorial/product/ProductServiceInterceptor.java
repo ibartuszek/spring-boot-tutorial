f00b6b60e320569e2ad4a17ab6f8e0577661e5da
Istvan Bartuszek <v-ibartuszek@hotels.com>
2019-05-20 15:22:16 +0200

    15 - REST: filter example

--- src/main/java/com/example/springboottutorial/product/ProductServiceInterceptor.java
+++ src/main/java/com/example/springboottutorial/product/ProductServiceInterceptor.java
@@ -12,23 +12,23 @@ import org.springframework.web.servlet.ModelAndView;
 @Component
 public class ProductServiceInterceptor implements HandlerInterceptor {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(ProductServiceInterceptor.class);
+    private final Logger logger = LoggerFactory.getLogger(ProductServiceInterceptor.class);
 
     @Override
     public boolean preHandle(final HttpServletRequest request, final HttpServletResponse response, final Object handler) throws Exception {
-        ProductServiceInterceptor.LOGGER.info("Pre handle method is calling.");
+        logger.info("Pre handle method is calling.");
         return true;
     }
 
     @Override
     public void postHandle(final HttpServletRequest request, final HttpServletResponse response, final Object handler,
             final ModelAndView modelAndView) throws Exception {
-        ProductServiceInterceptor.LOGGER.info("Post handle method is calling.");
+        logger.info("Post handle method is calling.");
     }
 
     @Override
     public void afterCompletion(final HttpServletRequest request, final HttpServletResponse response, final Object handler, final Exception ex)
             throws Exception {
-        ProductServiceInterceptor.LOGGER.info("Request and response is completed.");
+        logger.info("Request and response is completed.");
     }
 }
