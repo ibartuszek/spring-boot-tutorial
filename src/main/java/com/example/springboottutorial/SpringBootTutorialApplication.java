7d20a872fefa24b01f57a954c3ea022774c98003
Istvan Bartuszek <v-ibartuszek@hotels.com>
2019-05-22 10:47:31 +0200

    24 - Scheduler

--- src/main/java/com/example/springboottutorial/SpringBootTutorialApplication.java
+++ src/main/java/com/example/springboottutorial/SpringBootTutorialApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SpringBootTutorialApplication extends SpringBootServletInitializer implements ApplicationRunner {
 
     public static void main(String[] args) {
